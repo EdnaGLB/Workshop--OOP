a371b4151f0b64982376a54ff71a3011e7671fdb
SilverRaven <ria.lorro15@gmail.com>
2025-11-21 13:38:32 +0100

    Add Class Admin

---
+++ src/Domain/Users/Class_Admin.cs
@@ -0,0 +1,46 @@
+namespace Domain
+{
+    class Admin : User
+    {
+        public void AddItem()
+        {
+                //Add Code
+        }
+        public void RemoveItem()
+        {
+                //Add Code
+        }
+
+        public void ViewEmployee()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        }
+
+        public void CreateEmployee()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        }
+
+        public void DeleteEmployee()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        }
+
+        public void ViewBorrower()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        } 
+        public void CreateBorrower()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        }
+        public void DeleteBorrower()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        }
+        public void UpdatePersonalData()
+        {
+            //Add Code  (Need to create a List with all Employee)
+        }
+    }
+}
